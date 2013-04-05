4ffa7777844faaa2e7980bcf07a1d991c1cdf87a
hakugenn <hakugenn@hotmail.com>
2013-04-05 20:44:55 +0900

    added writing red dot.

--- my2DGraphics/my2DGraphics/main.cpp
+++ my2DGraphics/my2DGraphics/main.cpp
@@ -6,8 +6,9 @@ namespace GameLib{
 	void Framework::update(){
 		unsigned* vram = videoMemory();
 		static unsigned i;
-		vram[ i ] += i * 100;
-		i += 9973; //1–œˆÈ‰ºÅ‘å‚Ì‘f”
-		i %= ( width() * height() );
+		vram[ 200*width() + 100] = 0xff0000;
+		//vram[ i ] += i * 100;
+		//i += 9973; //1–œˆÈ‰ºÅ‘å‚Ì‘f”
+		//i %= ( width() * height() );
 	}
 }
